7669046d1f0ac2c9e7d872ed0ef773d925ce9789
xiaoyu <3344609717@qq.com>
2026-07-17 16:32:42 +0800

    initial commit

---
+++ src/main/java/org/xiaoyu/gitarena/GitArenaApplication.java
@@ -0,0 +1,13 @@
+package org.xiaoyu.gitarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitArenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitArenaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xiaoyu/gitarena/GitArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xiaoyu.gitarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitArenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
